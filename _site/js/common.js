33bb0e943095392c618b5fca685488cbd1a9d684
dpismenny <denis.pismenny@gmail.com>
2013-10-31 01:17:32 +0200

    new

--- _site/js/common.js
+++ _site/js/common.js
@@ -2,4 +2,30 @@ $(document).ready(function() {
 
 	hljs.initHighlightingOnLoad();
 
+	$(".js-scroll-btn").click(function (){
+		$('html, body').animate({
+			scrollTop: $("#js-scroll-to").offset().top
+		}, 500);
+	});
+
+	jQuery('.icon_add').click(function (event) {
+		jQuery('.social').slideToggle();
+	});
+
+});
+
+$(function() {
+
+	var footerHeight = $(".footer").height();
+	$(".out").css("margin-bottom", -footerHeight);
+	$(".push").css("height", footerHeight);
+
+});
+
+$(window).resize(function() {
+
+	var footerHeight = $(".footer").height();
+	$(".out").css("margin-bottom", -footerHeight);
+	$(".push").css("height", footerHeight);
+
 });
